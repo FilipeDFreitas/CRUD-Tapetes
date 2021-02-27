39b041fd9d19ca5b17e2581cea48b38c4e21cafd
Filipe de Freitas <filipefreitas.jedi@gmail.com>
2021-02-27 15:33:07 -0300

    Versão 0.6 versão seguindo feedback do professor *arraylist substituido por hashmap *variaveis renomedas para maior claridade de leitura *metodos não ficam reeviando todas a tabela a cada operação mais

--- src/main/java/vendatapetes/Controller.java
+++ src/main/java/vendatapetes/Controller.java
@@ -1,6 +1,8 @@
 package vendatapetes;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -8,45 +10,46 @@ import javax.swing.table.DefaultTableModel;
  * @author Filipe de Freitas Martins da Silva - GRR20193884
  */
 public class Controller {
+    private static HashMap<String, Cliente> capitalCities = new HashMap<String, Cliente>();
     private static ArrayList<Cliente> Clientes = new ArrayList<Cliente>();
     
-    public static Object[] incluirCliente(String jTextField1, String jTextField2, String jTextField3){
-        if(!jTextField3.equals("")){
-            Cliente c = new Cliente(jTextField1, jTextField2, jTextField3);
-            Clientes.add(c);
+    public static void incluirCliente(String nome, String sobrenome, String CPF){
+        if(!(CPF.equals("")||nome.equals("")||sobrenome.equals(""))){
+            Cliente c = new Cliente(nome, sobrenome, CPF);
+            capitalCities.put(CPF, c);
         }
-        return Clientes.toArray();
     }
-    public static Object[] atualizarCliente(String jTextField1, String jTextField2, String jTextField3){
-        for(Cliente c: Clientes){
-            if(c.getCPF().equals(jTextField3))
-                c.Alterar(jTextField1, jTextField2);
+    public static void atualizarCliente(String nome, String sobrenome, String CPF){
+        if(!CPF.equals("")){
+        Cliente alvo =capitalCities.get(CPF);
+        if(nome == ""){
+            nome = alvo.getNome();
         }
-        
-        return Clientes.toArray();
-    }
-    public static Object[] removerCliente(String jTextField1, String jTextField2, String jTextField3){
-        Cliente obj = new Cliente();
-        for(Cliente c: Clientes){
-            if(c.getCPF().equals(jTextField3))
-                obj=c;  
+        if(sobrenome == ""){
+            sobrenome = alvo.getSobrenome();
         }
-        Clientes.remove(obj);
-        return Clientes.toArray();
+        if(alvo!=null){
+        alvo.Alterar(nome, sobrenome);
+        capitalCities.put(CPF, alvo);
+        }
+        }
     }
-    public static Object[] listarCliente(Object[] obj){
+    public static void removerCliente(String CPF){
+        capitalCities.remove(CPF);
+    }
+    public static Object[] listarCliente(Object[] ClienteAlvo){
         ArrayList<Cliente> Busca= new ArrayList<Cliente>();
-        if(obj.length>3){
-        String busca=obj[0].toString();
-        String nome=obj[1].toString();
-        String sobrenome=obj[2].toString();
-        String cpf=obj[3].toString();
-        
-        for(Cliente c: Clientes){
-            if((nome.equals("true")&& c.getNome().matches("(.*)"+busca+"(.*)"))
-                    ||(sobrenome.equals("true")&& c.getSobrenome().matches("(.*)"+busca+"(.*)"))
-                    ||(cpf.equals("true")&& c.getCPF().matches("(.*)"+busca+"(.*)"))){ 
-                Busca.add(c);
+        if(ClienteAlvo.length>3){
+        String busca=ClienteAlvo[0].toString();
+        String nome=ClienteAlvo[1].toString();
+        String sobrenome=ClienteAlvo[2].toString();
+        String cpf=ClienteAlvo[3].toString();
+
+        for(Map.Entry<String, Cliente> c: capitalCities.entrySet()){
+            if((nome.equals("true")&& c.getValue().getNome().matches("(.*)"+busca+"(.*)"))
+                    ||(sobrenome.equals("true")&& c.getValue().getSobrenome().matches("(.*)"+busca+"(.*)"))
+                    ||(cpf.equals("true")&& c.getValue().getCPF().matches("(.*)"+busca+"(.*)"))){ 
+                Busca.add(c.getValue());
             }
         }
         if(busca.isEmpty())
